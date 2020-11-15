df362a73fe37d31152c2514087ffa75e4ddd2f33
Ananth <ananth@wix.com>
2020-11-14 19:49:52 -0600

    clean_dataset - very early stage

--- clean_dataset.py
+++ clean_dataset.py
@@ -1,1 +1,51 @@
-/home/analog/python/clean_dataset.py
+import pandas as pd
+import numpy as np
+
+def get_numeric_outliers( series ) :
+    """ pd.Series (truly numeric) --> dict of outlier values (key) and reasons (values )"""
+    raw_avg = np.mean( series )
+    elems = series.tolist()
+    uniques = series.unique().tolist()
+    uniques.sort()
+    done = False
+    outliers = []
+
+    # process from bottom up
+    while (not done and uniques) :
+        suspect = uniques[0]
+        uniques = uniques[1:]
+        subset = list( filter( (suspect).__ne__, elems) )  # all but this one
+        abs_dists = np.asarray( [abs(x-raw_avg) for x in subset ] )
+        # really need to DRY these into a separate function :(
+        if abs(suspect - raw_avg) > abs_dists.mean() + 3 * abs_dists.std() :
+            outliers.append( suspect )
+        else :
+            done = True   
+
+    done = False
+    # process from top down
+    while ( not done and uniques) :
+        suspect = uniques[-1]
+        uniques = uniques[:-1]
+        subset = list( filter( (suspect).__ne__, elems) )  # all but this one
+        abs_dists = np.asarray( [abs(x-raw_avg) for x in subset ] )
+        if suspect - raw_avg > abs_dists.mean() + 3 * abs_dists.std() :
+            outliers.append( suspect )
+        else :
+            done = True
+
+     
+    if len( outliers ) > 0 :
+        return { outlier : "Lower abs dist 3 sigma from mean-clean-avg-dist" for outlier in outliers }
+    else :
+        return None
+
+# given a dataframe, report the outliers in each column
+# default is preview mode. User has to set update=True
+def clean_dataset( df , update=False ) :
+    """ dataframe, bool --> nothing (strings printed)"""
+    for col in df.columns :
+        if df[col].dtypes == np.number :    # is numeric already
+            outs = get_numeric_outliers( df[col] )
+        else :
+            pass
--- clean_dataset_test.py
+++ clean_dataset_test.py
@@ -1,1 +1,23 @@
-/home/analog/python/clean_dataset_test.py
+from clean_dataset import *
+import numpy as np
+import pdb
+
+import unittest
+
+class Test_get_numeric_outliers( unittest.TestCase ) :
+
+    def test_basic(self) :
+        data = np.array([10,10,11,12,13,10,11,11,14,14,1,15,19,20,17,17,])
+        testcase = pd.Series( data )
+        expected = {1 : "Lower abs dist 3 sigma from mean-clean-avg-dist"}
+        self.assertEqual( get_numeric_outliers(testcase), expected )
+
+    def test_max(self) :
+        data = np.array([10,10,11,12,13,10,11,11,14,14,1,15,19,20,17,17,100])
+        testcase = pd.Series( data )
+        expected = {1 : "Lower abs dist 3 sigma from mean-clean-avg-dist",
+                    100 : ""}
+        self.assertEqual( get_numeric_outliers(testcase), expected )
+
+pdb.set_trace()
+unittest.main()
