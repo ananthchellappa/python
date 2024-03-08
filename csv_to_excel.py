1e137d3f0d37f36aebbd10d94187f30d28971866
ananthchellappa <ananth.chellappa@outlook.com>
2024-03-08 15:46:46 -0700

    add header name for index column

--- csv_to_excel.py
+++ csv_to_excel.py
@@ -17,7 +17,7 @@ df = pd.read_csv(fn)
 #Convert csv to excel
 writer = pd.ExcelWriter(fn[:-4]+'.xlsx', engine='xlsxwriter')
 df.to_excel(
-    writer, sheet_name='Testing', startrow=1, startcol=1)
+    writer, sheet_name='Testing', startrow=1, startcol=1, )
 
 #Load the worksheet object
 worksheet = writer.sheets['Testing']
@@ -26,7 +26,7 @@ worksheet = writer.sheets['Testing']
 (max_row, max_col) = df.shape
 
 # Create a list of column headers, to use in add_table().
-column_settings = [{'header': column} for column in df.columns]
+column_settings = [{'header': column} for column in ['#] + df.columns] # AC
 
 # Add the Excel table structure. Pandas will add the data.
 worksheet.add_table(1, 1, max_row+1, max_col+1, 
